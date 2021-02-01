d5d81ec53001c8c9f3628854b5e56b110c6098d0
div-gupta <65812330+div-gupta@users.noreply.github.com>
2021-02-01 22:53:11 +0530

    Delete question2.py

--- question2.py
+++
@@ -1,6 +0,0 @@
-dict1 = {"1" : "name1" , "2":"name2" ,"3":"name3" }
-dict2 = {"1" : 50 ,"2":60, "3":70}
-
-itemMaxValue = max(dict2.items(), key=lambda x : x[1])
-print('Key With Max value in Dict: ', itemMaxValue[0])
-print('Max value in Dict: ', itemMaxValue[1])
